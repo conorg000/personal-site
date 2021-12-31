35e2a00cd33ed5bec5dd554c7f2282184d1de8f0
Conor Gould <conor.gould@griffithuni.edu.au>
2021-12-31 13:53:30 +1100

    Changed css

--- src/App.tsx
+++ src/App.tsx
@@ -6,18 +6,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Coming soon...</h1>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Conor's little corner of the internet
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
